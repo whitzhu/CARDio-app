72420a609af101e1349f3ccd58a04a2329705400
jasminemina <mina.jas24@gmail.com>
2017-04-28 13:43:09 -0700

    Add form to createDeckView for customized deck feature

--- CARDio/client/components/CreateDeckView.js
+++ CARDio/client/components/CreateDeckView.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Card, Button } from 'react-native-elements'
+import { Card, Button, FormLabel, FormInput, FormValidationMessage } from 'react-native-elements'
 import { navigationOptions } from 'react-navigation'
 
 export default class CreateDeckView extends React.Component {
@@ -11,6 +11,18 @@ export default class CreateDeckView extends React.Component {
     	<Card
     		title='Create your own deck'
     		>
+    		<FormLabel>Workout 1</FormLabel>
+				<FormInput />
+				<FormLabel>Workout 2</FormLabel>
+				<FormInput />
+				<FormLabel>Workout 3</FormLabel>
+				<FormInput />
+				<FormLabel>Workout 4</FormLabel>
+				<FormInput />
+				<Button
+					title='Save Deck'
+					onPress={() => navigate('SelectDeckView')}
+					/>
 	    </Card>
 	  )
 	}
